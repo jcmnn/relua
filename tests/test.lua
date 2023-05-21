087628629601e38619b25b228ba4b8eac1131149
jcmnn <jacobjm18@gmail.com>
2023-05-21 13:50:20 -0400

    Add lua files

---
+++ tests/test.lua
@@ -0,0 +1,20 @@
+if a == 23 and f == 43 then
+    print('yes')
+else
+    print('no')
+end
+--[[
+function x(test1, test2)
+    function y(...)
+        print(...)
+    end
+
+    x = 32
+    y("hello")
+    y(true)
+    y(3.4)
+end
+
+
+x('abc', false)
+]]
